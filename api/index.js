f45a9bb9486263be61c5f2572f366e19df087690
ashleyperrett <61692449+ashleyperrett@users.noreply.github.com>
2020-04-14 12:51:13 +0100

    added circle ci and basic project setup

--- api/index.js
+++ api/index.js
@@ -1,20 +1,10 @@
-require('module-alias/register');
-require('dotenv').config();
-const serverless = require('serverless-http');
-const express = require('express');
-const path = require('path');
-const app = express();
+const getDoc = async (event) => {
+  return {
+    statusCode: 200,
+    body: "Hello World"
+  }
+}
 
-app.use(function(req, res, next) {
-  // had to rewrite the path to get it playing nice with a not-root resource in api gateway
-  req.url = req.url.replace(`/${process.env.URL_PREFIX}`, '');
-  next();
-});
-
-app.get('/', async (req, res) => {
-  res.send('Hello World');
-});
-
-module.exports.handler = serverless(app, {
-  binary: ['*/*']
-});
+module.exports = {
+  getDoc
+}
