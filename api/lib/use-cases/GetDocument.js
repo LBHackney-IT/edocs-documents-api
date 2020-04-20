01cc482d9b25bd5d7a6eb43fc2df0237a72c19fc
Ninamma <ninamma.rai@outlook.com>
2020-04-20 16:18:30 +0100

    Check if document is already cached

--- api/lib/use-cases/GetDocument.js
+++ api/lib/use-cases/GetDocument.js
@@ -5,19 +5,22 @@ module.exports = function(options) {
   const s3Gateway = options.s3Gateway;
 
   return async function(documentId) {
-    const outputDoc = await edocsGateway.getDocument(documentId);
-    const mimeType = mimeTypes.extension(outputDoc.headers["content-type"]);
-    doc = {
-      mimeType,
-      doc: outputDoc.body,
-      filename: `${documentId}.${mimeType}`
+    let doc = await s3Gateway.get(documentId);
+
+    if (!doc) {
+      const outputDoc = await edocsGateway.getDocument(documentId);
+      const mimeType = mimeTypes.extension(outputDoc.headers["content-type"]);
+      doc = {
+        mimeType,
+        doc: outputDoc.body,
+        filename: `${documentId}.${mimeType}`
+      }
+      
+      await s3Gateway.put(documentId, doc)
+  
+      doc.url = await s3Gateway.getUrl(documentId, doc.mimeType, doc.mimeType)
     }
     
-    // Cash into an S3 bucket:
-    await s3Gateway.put(documentId, doc)
-
-    doc.url = await s3Gateway.getUrl(documentId, doc.mimeType, doc.mimeType)
-    // TODO: return doc url from bucket:
     return doc;
   };
 };
