d53732676584087ea5f457a0fec6de98af1b3151
csabagyorfi <csaba@madetech.com>
2020-04-20 12:32:33 +0100

    Add caching document to s3 bucket.
    Co-authored-by: csabagyorfi <csaba@madetech.com>

--- api/test/use-cases/GetDocument.test.js
+++ api/test/use-cases/GetDocument.test.js
@@ -12,26 +12,52 @@ const createEdocsGatewaySpy = (document, contentType) => {
   };
 
 const createS3GatewaySpy = document => {
-  //TODO Spy on S3 put request
   return {
     put: jest.fn(() => {
+      return {
+        promise: () => {
+          return Promise.resolve;
+        }
+      };
+    }),
+    getUrl: jest.fn(() => {
       return 'http://dummy-url.com/?';
     })
   };
 }
 
+
 describe('GetDocument', function() {
-  it.only('gets the right content', async function() {
+  it('gets the right content', async function() {
     const documentId = 1234;
     const document = 'some document';
     const edocsGatewaySpy = createEdocsGatewaySpy(document, 'text/xml');
-    const usecase = GetDocument({ edocsGateway: edocsGatewaySpy, s3Gateway: null });
+    const usecase = GetDocument({ edocsGateway: edocsGatewaySpy, s3Gateway: createS3GatewaySpy()});
     const attachment = await usecase(documentId);
 
     expect(edocsGatewaySpy.getDocument).toHaveBeenCalledTimes(1);
     expect(attachment.doc).toBe(document);
     expect(attachment.mimeType).toBe('xml')
     expect(attachment.filename).toBe('1234.xml')
+  });
+
+  it('caches into an S3 bucket and returns the URL from S3', async function() {
+    const documentId = 1234;
+    const document = 'some document';
+    const edocsGatewaySpy = createEdocsGatewaySpy(document, 'text/xml');
+    const s3GatewaySpy = createS3GatewaySpy()
+    const usecase = GetDocument({ edocsGateway: edocsGatewaySpy, s3Gateway: s3GatewaySpy });
+  
+    const attachment = await usecase(documentId);
+    
+    expect(edocsGatewaySpy.getDocument).toHaveBeenCalledTimes(1);
+    // expect(s3GatewaySpy.put).toHaveBeenCalledWith(documentId, {
+    //   mimeType: 'xml',
+    //   doc: document,
+    //   filename: '1234.xml',
+    // });
+    expect(attachment.doc).toBe(document);
+    expect(attachment.url).toBe('http://dummy-url.com/?')
   });
 })
  
