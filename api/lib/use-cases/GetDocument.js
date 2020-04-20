83df310c364965b1b08be6c0db4002298d7a977b
csabagyorfi <csaba@madetech.com>
2020-04-20 12:32:33 +0100

    Add caching document to s3 bucket.
    Co-authored-by: csabagyorfi <csaba@madetech.com>

--- api/lib/use-cases/GetDocument.js
+++ api/lib/use-cases/GetDocument.js
@@ -2,20 +2,22 @@ const mimeTypes = require("mime-types");
 
 module.exports = function(options) {
   const edocsGateway = options.edocsGateway;
-  // const s3Gateway = options.s3Gateway;
+  const s3Gateway = options.s3Gateway;
 
   return async function(documentId) {
     const outputDoc = await edocsGateway.getDocument(documentId);
     const mimeType = mimeTypes.extension(outputDoc.headers["content-type"]);
-
-    // Cash into an S3 bucket:
-    // s3Gateway.put(documentId, outputDoc)
-
-    // TODO: return doc url from bucket:
-    return {
+    doc = {
       mimeType,
       doc: outputDoc.body,
       filename: `${documentId}.${mimeType}`
-    };
+    }
+    
+    // Cash into an S3 bucket:
+    await s3Gateway.put(documentId, doc)
+
+    doc.url = await s3Gateway.getUrl(documentId, doc.mimeType, doc.mimeType)
+    // TODO: return doc url from bucket:
+    return doc;
   };
 };
