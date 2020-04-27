dfb9bed9bcd394ed11e3cc022cc9f8b53e851fc5
Ninamma <ninamma.rai@outlook.com>
2020-04-27 17:33:15 +0100

    Add converrting to get document@

--- api/lib/use-cases/ConvertDocument.js
+++ api/lib/use-cases/ConvertDocument.js
@@ -1,15 +1,15 @@
 module.exports = function(options) {
-
    return async function(filePath,outputdir) {
            //const {unpack, defaultArgs} = require('@shelf/aws-lambda-libreoffice');
     //await unpack({inputPath: INPUT_PATH});
     //await unpack(); // default path /tmp/instdir/program/soffice.bin
+
        var execSync = require('child_process').execSync;
        
-       execSync(
+      execSync(
            `/usr/local/bin/soffice --convert-to pdf ${filePath} --outdir ${outputdir}`
            );
-        return outputdir;
+      return outputdir;
    };
 
 };
--- api/lib/use-cases/GetDocument.js
+++ api/lib/use-cases/GetDocument.js
@@ -1,4 +1,14 @@
 const mimeTypes = require("mime-types");
+const fs = require('fs');
+
+//TODO: get better name for this func
+function saveFileLocally(docBody, fileName) {
+  fs.writeFile(`${process.env.TEMP_FILE_PATH}${fileName}`, docBody)
+  console.log('The file has been saved!');
+  return `${process.env.TEMP_FILE_PATH}${fileName}`
+}
+
+const convertDocument = require('./ConvertDocument')
 
 module.exports = function(options) {
   const edocsGateway = options.edocsGateway;
@@ -16,11 +26,17 @@ module.exports = function(options) {
         const mimeType = outputDoc.headers["content-type"];
         const extension = mimeTypes.extension(mimeType);
 
+        filePath = saveFileLocally(outputDoc.body, `${documentId}.${extension}`)
+
+        wherePDFis = await convertDocument(filePath, process.env.TEMP_FILE_PATH)
+
+        hopefullyPDF = fs.readFile(`${process.env.TEMP_FILE_PATH}${documentId}.pdf`)
+
         doc = {
           mimeType,
           extension,
-          doc: outputDoc.body,
-          filename: `${documentId}.${extension}`
+          doc: hopefullyPDF,
+          filename: `${documentId}.pdf`
         };
 
         await s3Gateway.put(documentId, doc);
