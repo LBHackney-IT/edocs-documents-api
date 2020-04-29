a96c38ee6e31a1a6b79e9a3d3327c7f1e8b2c4de
Ninamma <ninamma.rai@outlook.com>
2020-04-29 10:30:59 +0100

    log tmp contents

--- api/lib/use-cases/ConvertDocument.js
+++ api/lib/use-cases/ConvertDocument.js
@@ -6,10 +6,13 @@ module.exports = async function(fileName) {
       const {unpack, defaultArgs} = require('@shelf/aws-lambda-libreoffice');
       await unpack({inputPath: INPUT_PATH}); // default path /tmp/instdir/program/soffice.bin
       soffice = '/tmp/instdir/program/soffice.bin'
+
     }
 
     var execSync = require("child_process").execSync;
-
+    execSync(
+      'cd /tmp && ll'
+    )
     execSync(
       `${soffice} --headless --convert-to pdf /tmp/${fileName} --outdir /tmp`
     );
